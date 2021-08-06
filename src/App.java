16ae49ec7dd7c829d5ba94525f794b0f8510c67a
John <john@r73.de>
2021-08-06 18:23:16 +0200

    -1 ist nicht gefunden :)

--- src/App.java
+++ src/App.java
@@ -1,66 +1,62 @@
 public class App {
-    
+
     public static void main(String[] args) {
-        //System.out.println("Hello, World!");
-        Parkdeck<Fahrzeug> pd01 = new Parkdeck<>(); //je 30 plätze
+        // System.out.println("Hello, World!");
+        Parkdeck<Fahrzeug> pd01 = new Parkdeck<>(); // je 30 plätze
         Parkdeck<Fahrzeug> pd02 = new Parkdeck<>();
         Parkdeck<Fahrzeug> pd03 = new Parkdeck<>();
-        Parkdeck<Fahrzeug>[] haus =  (Parkdeck<Fahrzeug>[]) new Parkdeck[3];
-        haus[0]= pd01;
-        haus[1]= pd02;
-        haus[2]= pd03;
+        Parkdeck<Fahrzeug>[] haus = (Parkdeck<Fahrzeug>[]) new Parkdeck[3];
+        haus[0] = pd01;
+        haus[1] = pd02;
+        haus[2] = pd03;
         Parkhaus ph = new Parkhaus(haus);
         Fahrzeug blauerOpel = new Auto("BN FN 2");
+        System.out.println(ph.woIstFahrzeug(blauerOpel));
+
         ph.einparken(blauerOpel);
-       // ph.printFreiePlaetze();
+        // ph.printFreiePlaetze();
         System.out.println("Parkhaus 1:");
         System.out.println(ph);
-        try{
 
-            System.out.println(ph.woIstFahrzeug(blauerOpel));
-        }catch(Exception e){
-            System.out.println("e"); }
+        System.out.println(ph.woIstFahrzeug(blauerOpel));
+
     }
 
-    //Struktur
-    //Autohaus: (Parkhaus)
-    //Zwei etagen
-    //Autos auf etage 1
-    //Motorräder auf etage 2
-    //Mopeds auf etage 3
-    //Behinderten-Parkplatz
+    // Struktur
+    // Autohaus: (Parkhaus)
+    // Zwei etagen
+    // Autos auf etage 1
+    // Motorräder auf etage 2
+    // Mopeds auf etage 3
+    // Behinderten-Parkplatz
 
-
-    //Klassen:
-    //Parkplatz
-    //Parkdeck
-    //Parkhaus
-    //Fahrzeug
-    //Zweirad +-+
-    //Auto +-+
+    // Klassen:
+    // Parkplatz
+    // Parkdeck
+    // Parkhaus
+    // Fahrzeug
+    // Zweirad +-+
+    // Auto +-+
 
     // +-+ = HatBehindertenAusweis
-    //(Parkdauer) Park-Start-Zeit
+    // (Parkdauer) Park-Start-Zeit
 
-    //Fahrzeug Parken
-    //Freie Plätze anzeigen
-    //Kosten anzeigen
-    //Fahzeug (AusParken)
+    // Fahrzeug Parken
+    // Freie Plätze anzeigen
+    // Kosten anzeigen
+    // Fahzeug (AusParken)
 
-    //Uebungen:
-    //Tests schreiben, wird der richtige fehler gegeben wenn ein parkdeck voll ist?
+    // Uebungen:
+    // Tests schreiben, wird der richtige fehler gegeben wenn ein parkdeck voll ist?
 
-
-    //Parkhausausgabe via Iterator PD01 Tostring usw
-    //Parkdeckausgabe via Iterator Parkplatz07 Tostring
-    //Parkplatzausgabe via Kennzeichen Tostring + Kosten Tostring
-
+    // Parkhausausgabe via Iterator PD01 Tostring usw
+    // Parkdeckausgabe via Iterator Parkplatz07 Tostring
+    // Parkplatzausgabe via Kennzeichen Tostring + Kosten Tostring
 
     // Deck 0:
     // {"BN LO 87", 5.75 Eur},{ "SU WG 50", 17.00 Eur}
     // Deck 1:
     // {"BN LO 87", 5.75 Eur},{ null, 0.00 Eur}
-    
 
-    //Instanceof beim Einparken testen
+    // Instanceof beim Einparken testen
 }
--- src/Parkhaus.java
+++ src/Parkhaus.java
@@ -2,50 +2,50 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.NoSuchElementException;
 
-
 public class Parkhaus {
     Parkdeck<Fahrzeug>[] gebaeude;
-    
+
     Parkhaus(Parkdeck<Fahrzeug>[] haus) {
         this.gebaeude = haus;
     }
 
-    public boolean einparken(Fahrzeug einparker){
-        if(this.gebaeude[0]== null){
+    public boolean einparken(Fahrzeug einparker) {
+        if (this.gebaeude[0] == null) {
             System.out.println("fehler");
         }
         return this.gebaeude[0].einparken(einparker);
     }
-    public boolean istEingeparkt(Fahrzeug anders){
-       for (int i = 0; i < gebaeude.length; i++) {
-          if(gebaeude[i].kennzeichenSuche(anders.kennzeichen)){
-            return true;
-          }
+
+    public boolean istEingeparkt(Fahrzeug anders) {
+        for (int i = 0; i < gebaeude.length; i++) {
+            if (gebaeude[i].kennzeichenSuche(anders.kennzeichen)) {
+                return true;
+            }
         }
         return false;
     }
-    public int woIstFahrzeug(Fahrzeug a) throws NoSuchAlgorithmException{
-        int erg = 0;
-        if(!istEingeparkt(a)){
-            throw new NoSuchElementException("Das Auto steht nicht im Parkhaus.");
-        }else{
-            for (int i = 0; i < gebaeude.length;i++) {
-                erg = gebaeude[i].fahrzeugSuche(a);
-                if(erg != -1){
-                   System.out.println("Parkdeck: "+i);
-                   return erg;
-                }
-              }
+
+    public int woIstFahrzeug(Fahrzeug a) {
+        int erg = -1;
+
+        for (int i = 0; i < gebaeude.length; i++) {
+            erg = gebaeude[i].fahrzeugSuche(a);
+            if (erg != -1) {
+                System.out.println("Parkdeck: " + i);
+                return erg;
+            }
         }
-        return erg;
+
+        return erg;// Hier -1
     }
 
     public void printFreiePlaetze() {
-        for(Parkdeck<Fahrzeug> a : gebaeude){
+        for (Parkdeck<Fahrzeug> a : gebaeude) {
             System.out.println(a.getFreiePlaetze());
         }
     }
-    public String toString(){
-        return gebaeude[0].toString()+ "\n" + gebaeude[1].toString() + "\n" + gebaeude[2].toString();
+
+    public String toString() {
+        return gebaeude[0].toString() + "\n" + gebaeude[1].toString() + "\n" + gebaeude[2].toString();
     }
 }
