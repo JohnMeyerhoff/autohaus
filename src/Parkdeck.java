ed67750cace27fed2cf55e4fc7d6cb68b015adf6
Klara-g <Klara-g@users.noreply.github.com>
2021-08-06 18:00:58 +0200

    Update Parkdeck.java

--- src/Parkdeck.java
+++ src/Parkdeck.java
@@ -65,7 +65,7 @@ public class Parkdeck<T extends Fahrzeug> implements Iterable<Parkplatz>{
         int gefunden = 0;;
         for(int i = 0; i < size; i++){
             if(!(b.kennzeichen.equals(parkflaeche[i].belegtDurchFahrzeug.kennzeichen))){
-                gefunden = 0;
+                gefunden = -1;
             }else{
                 gefunden = i;
             }
