97e5e77e105a59454ae6b8533ce4b2b683cd32c0
Klara-g <Klara-g@users.noreply.github.com>
2021-08-09 17:04:54 +0200

    Update Test.java

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class Test {
+   @BeforeEach
+   void before(){
+
+   } 
+   @AfterEach
+   void after(){
+       
+   }
+    //Das fahrzeug nimmt einen Patz ein, wir ahben 90 plätze. 80 sollte passen. 
+    // Generieren sie mit eienr schelife 80 austos die sie einpraken. Gebe denen
+  //kein Varialennamen. 
+}
