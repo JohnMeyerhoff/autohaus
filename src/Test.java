e163d0aee931928a04310e66202f14f2cff42a17
Klara-g <Klara-g@users.noreply.github.com>
2021-08-09 17:13:28 +0200

    Create Test.java

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class Test {
+   @BeforeEach
+   void before(){
+    Parkdeck<Fahrzeug> etage = new Parkdeck<>();
+    Parkhaus erstes = new Parkhaus<>(haus);
+   } 
+   @AfterEach
+   void after(){
+
+   }
+    //Das fahrzeug nimmt einen Platz ein, wir haben 90 plätze. 80 sollte passen. 
+    // Generieren sie mit eienr schelife 80 autos die sie einparken. Gebe denen
+  //kein Varialennamen. 
+}
