f7981acda2f79d597e7f220d3c020fea6f3e8b03
John <john@r73.de>
2021-08-09 17:16:26 +0200

    move

--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,1 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 
-public class Test {
-   @BeforeEach
-   void before(){
-    Parkdeck<Fahrzeug> etage = new Parkdeck<>();
-    Parkhaus<Fahrzeug> erstes = new Parkhaus<>(etage);
-   } 
-   @AfterEach
-   void after(){
-
-   }
-    //Das fahrzeug nimmt einen Platz ein, wir haben 90 plätze. 80 sollte passen. 
-    // Generieren sie mit eienr schelife 80 autos die sie einparken. Gebe denen
-  //kein Varialennamen. 
-}
--- src/tests/Test.java
+++ src/tests/Test.java
@@ -1,17 +1,19 @@
 package tests;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import Parkdeck;
 
 public class Test {
    @BeforeEach
    void before(){
-
+    Parkdeck<Fahrzeug> etage = new Parkdeck<>();
+    Parkhaus<Fahrzeug> erstes = new Parkhaus<>(etage);
    } 
    @AfterEach
    void after(){
-       
+
    }
-    //Das fahrzeug nimmt einen Patz ein, wir ahben 90 plätze. 80 sollte passen. 
-    // Generieren sie mit eienr schelife 80 austos die sie einpraken. Gebe denen
-  //kein Varialennamen. 
+    //Das fahrzeug nimmt einen Platz ein, wir haben 90 plätze. 80 sollte passen. 
+    // Generieren sie mit eienr schelife 80 autos die sie einparken. Gebe denen
+    //kein Varialennamen. 
 }
