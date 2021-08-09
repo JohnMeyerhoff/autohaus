8a554d30ce06c96d5a0cb9876bcf9753bfb52ec9
Klara-g <Klara-g@users.noreply.github.com>
2021-08-09 17:28:39 +0200

    Create NeuerTest.java

---
+++ src/parkhaus/NeuerTest.java
@@ -0,0 +1,27 @@
+package parkhaus;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+public class NeuerTest {
+
+   @BeforeEach
+   void before(){
+    Parkdeck<Fahrezug>[] etage 
+    Parkhaus<Fahrzeug> erstes = new Parkhaus<>(etage);
+   } 
+   @AfterEach
+   void after(){
+
+   }
+    //Das fahrzeug nimmt einen Platz ein, wir haben 90 plätze. 80 sollte passen. 
+    // Generieren sie mit eienr schelife 80 autos die sie einparken. Gebe denen
+    //kein Varialennamen. 
+
+    @Test
+    public void einfach(){
+    
+    }
+}
+
+}
