fabf0912d9f74a14054e56919ab7017e4534a584
John <john@r73.de>
2021-08-11 17:36:23 +0200

    Aufgabe

--- src/parkhaus/tests/BasicTest.java
+++ src/parkhaus/tests/BasicTest.java
@@ -8,6 +8,8 @@ public class BasicTest {
     /*
     Vorgehen:#
     Neuen Test (Methode) anlegen
+    Brauche ich weitere imports?
+    Kann ich "Scaffolding" aus anderen Tests übernehmen?
     Erwartungswert ermitteln
         - Erwartungs-Output (expected) für spezifischen Input
     Programm / Methodenaufruf durchführen
@@ -16,6 +18,39 @@ public class BasicTest {
         - Wie erhalte ich mein Ergebnis, wie berechnet sich mein Erwartungswert
     */
 
+
+    /*
+    Vorgehen:#
+    Neuen Test (Methode) anlegen
+    Brauche ich weitere imports?
+    Kann ich "Scaffolding" aus anderen Tests übernehmen?
+    Erwartungswert ermitteln
+        - Erwartungs-Output (expected) für spezifischen Input
+    Programm / Methodenaufruf durchführen
+        - Welche Methode wird aufgerufen
+    Gespeichertes Ergebnis und Erwartungswert vergleichen (assertEquals)
+        - Wie erhalte ich mein Ergebnis, wie berechnet sich mein Erwartungswert
+    */
+    //nullForHigherDeck(): Wird beim versuch ein Fahrzeug aus einem Parkdeck
+    //auszuparken welches nicht existiert null zurückgegeben?
+
+
+
+
+    /*
+    Vorgehen:#
+    Neuen Test (Methode) anlegen
+    Brauche ich weitere imports?
+    Kann ich "Scaffolding" aus anderen Tests übernehmen?
+    Erwartungswert ermitteln
+        - Erwartungs-Output (expected) für spezifischen Input
+    Programm / Methodenaufruf durchführen
+        - Welche Methode wird aufgerufen
+    Gespeichertes Ergebnis und Erwartungswert vergleichen (assertEquals)
+        - Wie erhalte ich mein Ergebnis, wie berechnet sich mein Erwartungswert
+    */
+    //nullForNotInsideFlaeche(): Wird beim versuch ein Fahrzeug aus einem Parkplatz
+    //auszuparken welcher nicht existiert null zurückgegeben?
 
    
 
