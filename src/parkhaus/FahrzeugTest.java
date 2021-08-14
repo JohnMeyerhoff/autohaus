df1887b64fbfb3023014e331af03cd4b060a3809
John <john@r73.de>
2021-08-14 17:02:45 +0200

    Create FahrzeugTest.java

---
+++ src/parkhaus/FahrzeugTest.java
@@ -0,0 +1,32 @@
+package parkhaus;
+
+import org.junit.jupiter.api.Test;
+
+public class FahrzeugTest {
+    /*
+     * testCompareTo(): Stellen sie sicher, dass mehrere Fahrzeuge durch den
+     * compareTo Aufruf gleich oder ungleich sind, je nach kennzeichen, unabhängig
+     * von ihrer Klasse. verwenden sie ein Zweirad ein Auto und zwei Fahrzeuge,
+     * vergleichen sie diese untereinander.
+     */
+
+    @Test
+    void testCompareTo() {
+
+    }
+
+    @Test
+    void testGetHatBehindertenAusweis() {
+
+    }
+
+    @Test
+    void testGetKennzeichen() {
+
+    }
+
+    @Test
+    void testToString() {
+
+    }
+}
