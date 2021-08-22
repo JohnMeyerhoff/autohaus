dc38ed42e74ef4c302d4354b9063154c4deeecba
klara <firstemail@icloud.com>
2021-08-22 14:29:39 +0200

    das ist so perfekt
    bitte nicht ändern

--- src/parkhaus/ParkdeckHelper.java
+++ src/parkhaus/ParkdeckHelper.java
@@ -18,4 +18,21 @@ public class ParkdeckHelper {
         }
         return res;
     }
+    //Es ist nicht Ziel dieser Methoden möglichst optimiert zu sein. Sie dürfen parkdeckToArray benutzen. 
+    public static <T extends Fahrzeug> String[] parkDeckGeradeToArray(Parkdeck<T> x) {
+        String[] alle = parkDeckToArray(x);
+        String[] links = new String[(alle.length)/2];
+        //x wird nicht mehr verwendet und zurückgegben werden soll eine Kopie von alle/abgeänderte Form von alle, die nur noch
+        //die geraden Parkplätze enthält
+       
+        int i = 0;
+        for( int f = 0; i < alle.length;f++){
+            links[f] = alle[i];
+            i = i + 2;
+        }
+        return links;
+    }
+    
+    public static <T extends Fahrzeug> String[] parkDeckUngeradeToArray(Parkdeck<T> pd) {
+    }
 }
